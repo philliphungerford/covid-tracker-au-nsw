bc2068b5435f854ca1ccc44e438df8d61ed14c8c
Phillip Hungerford <phillip.hungerford@gmail.com>
2021-08-17 11:37:10 +1000

    Added source information and fixed cumulative box overlapping with tabs

--- app.R
+++ app.R
@@ -89,9 +89,11 @@ ui <- dashboardPage(
       tabItem(tabName = "overview",
 
               #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              h1("COVID-19 Tracker NSW"),
+              p("This dashboard shows current statistics for COVID-19 cases in NSW. Data is sourced from", a("NSW Health.", href="https://www.health.nsw.gov.au/Infectious/covid-19/Pages/stats-nsw.aspx")),
+              #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
               h1("Overview"),
               # Info boxes for Overview
-              
               # DATE
               fluidRow(
                 valueBox(
